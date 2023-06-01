dec97132225585a32d166b0a67a320df095a6435
Taner Bajrovic <tanerbajrovic@gmail.com>
2023-06-01 10:56:22 +0200

    Create Version2 migration to reflect model updates

---
+++ FitnessFusion/FitnessFusion/Data/Migrations/20230601083420_Version2.cs
@@ -0,0 +1,498 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FitnessFusion.Data.Migrations
+{
+    public partial class Version2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CreditCards_Users_IDUser",
+                table: "CreditCards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rating_Users_IDUser",
+                table: "Rating");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Trainers_IDTrainer",
+                table: "Results");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Users_IDUser",
+                table: "Results");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_Trainers_IDTrainer",
+                table: "Schedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_Users_IDUser",
+                table: "Schedules");
+
+            migrationBuilder.DropTable(
+                name: "Trainers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_IDTrainer",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_IDUser",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Results_IDTrainer",
+                table: "Results");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Results_IDUser",
+                table: "Results");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rating_IDUser",
+                table: "Rating");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CreditCards_IDUser",
+                table: "CreditCards");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrainerId",
+                table: "Schedules",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Schedules",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrainerId",
+                table: "Results",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Results",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Rating",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "CreditCards",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ActivityCoefficient",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Experience",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IDGymProgram",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sex",
+                table: "AspNetUsers",
+                type: "nvarchar(1)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_TrainerId",
+                table: "Schedules",
+                column: "TrainerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_UserId",
+                table: "Schedules",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_TrainerId",
+                table: "Results",
+                column: "TrainerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_UserId",
+                table: "Results",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rating_UserId",
+                table: "Rating",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CreditCards_UserId",
+                table: "CreditCards",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_IDGymProgram",
+                table: "AspNetUsers",
+                column: "IDGymProgram");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_GymPrograms_IDGymProgram",
+                table: "AspNetUsers",
+                column: "IDGymProgram",
+                principalTable: "GymPrograms",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CreditCards_AspNetUsers_UserId",
+                table: "CreditCards",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rating_AspNetUsers_UserId",
+                table: "Rating",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_AspNetUsers_TrainerId",
+                table: "Results",
+                column: "TrainerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_AspNetUsers_UserId",
+                table: "Results",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_AspNetUsers_TrainerId",
+                table: "Schedules",
+                column: "TrainerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_AspNetUsers_UserId",
+                table: "Schedules",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_GymPrograms_IDGymProgram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CreditCards_AspNetUsers_UserId",
+                table: "CreditCards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rating_AspNetUsers_UserId",
+                table: "Rating");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_AspNetUsers_TrainerId",
+                table: "Results");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_AspNetUsers_UserId",
+                table: "Results");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_AspNetUsers_TrainerId",
+                table: "Schedules");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_AspNetUsers_UserId",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_TrainerId",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_UserId",
+                table: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Results_TrainerId",
+                table: "Results");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Results_UserId",
+                table: "Results");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rating_UserId",
+                table: "Rating");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CreditCards_UserId",
+                table: "CreditCards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_IDGymProgram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TrainerId",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "TrainerId",
+                table: "Results");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Results");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Rating");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "CreditCards");
+
+            migrationBuilder.DropColumn(
+                name: "ActivityCoefficient",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Experience",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IDGymProgram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Sex",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "Trainers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Experience = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sex = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainers", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ActivityCoefficient = table.Column<double>(type: "float", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IDGymProgram = table.Column<int>(type: "int", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sex = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Users_GymPrograms_IDGymProgram",
+                        column: x => x.IDGymProgram,
+                        principalTable: "GymPrograms",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_IDTrainer",
+                table: "Schedules",
+                column: "IDTrainer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_IDUser",
+                table: "Schedules",
+                column: "IDUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_IDTrainer",
+                table: "Results",
+                column: "IDTrainer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_IDUser",
+                table: "Results",
+                column: "IDUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rating_IDUser",
+                table: "Rating",
+                column: "IDUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CreditCards_IDUser",
+                table: "CreditCards",
+                column: "IDUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IDGymProgram",
+                table: "Users",
+                column: "IDGymProgram");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CreditCards_Users_IDUser",
+                table: "CreditCards",
+                column: "IDUser",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rating_Users_IDUser",
+                table: "Rating",
+                column: "IDUser",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Trainers_IDTrainer",
+                table: "Results",
+                column: "IDTrainer",
+                principalTable: "Trainers",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Users_IDUser",
+                table: "Results",
+                column: "IDUser",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_Trainers_IDTrainer",
+                table: "Schedules",
+                column: "IDTrainer",
+                principalTable: "Trainers",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_Users_IDUser",
+                table: "Schedules",
+                column: "IDUser",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
