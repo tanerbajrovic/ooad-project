059091dd3b572fa76b109f078ed87bdf7e50de1e
Zerina Ahmetovic <85384002+inferno73@users.noreply.github.com>
2023-06-06 09:01:22 +0200

    Modified ResultBuilder

--- FitnessFusion/FitnessFusion/Models/ResultBuilder.cs
+++ FitnessFusion/FitnessFusion/Models/ResultBuilder.cs
@@ -4,95 +4,95 @@ namespace FitnessFusion.Models.BuilderDP
 {
     public class ResultBuilder : IBuilder
     {
-        private Result _result = new Result();
+        private Result result = new Result();
         public void AddIDTrainer(int id)
         {
-            _result.IDTrainer = id;
+            result.IDTrainer = id;
         }
         public void AddAbsTest(int absTest)
         {
-            _result.AbsTest = absTest;
+            result.AbsTest = absTest;
         }
 
         public void AddBenchPress(double benchPress)
         {
-            _result.BenchPress = benchPress;
+            result.BenchPress = benchPress;
         }
 
         public void AddCalories(int calories)
         {
-            _result.Calories = calories;
+            result.Calories = calories;
         }
 
         public void AddDate(DateTime date)
         {
-            _result.Date = date;
+            result.Date = date;
         }
 
         public void AddHeight(double height)
         {
-            _result.Height = height;
+            result.Height = height;
         }
         public void AddArmCircumference(double armCircumference)
         {
-            _result.ArmCircumference = armCircumference;
+           result.ArmCircumference = armCircumference;
         }
         public void AddHipsCircumference(double hipsCircumference)
         {
-            _result.HipsCircumference = hipsCircumference;
+            result.HipsCircumference = hipsCircumference;
         }
         public void AddWaistCircumference(double waistCircumference)
         {
-            _result.WaistCircumference = waistCircumference;
+            result.WaistCircumference = waistCircumference;
         }
 
         public void AddLegCircumference(double legCircumference)
         {
-            _result.LegCircumference = legCircumference;
+            result.LegCircumference = legCircumference;
         }
 
         public void AddMass(double mass)
         {
-            _result.Mass = mass;
+            result.Mass = mass;
         }
 
         public void AddNeckCircumference(double neckCircumference)
         {
-            _result.NeckCircumference = neckCircumference;
+            result.NeckCircumference = neckCircumference;
         }
 
         public void AddPushUps(int pushUps)
         {
-            _result.PushUps = pushUps;
+            result.PushUps = pushUps;
         }
 
         public void AddRunning(double running)
         {
-            _result.Running = running;
+            result.Running = running;
         }
 
         public void AddSprintRunning(double sprintRunning)
         {
-            _result.SprintRunning = sprintRunning;
+            result.SprintRunning = sprintRunning;
         }
 
         public void AddSquat(double squat)
-        {
-            _result.Squat = squat;
+        {   
+            result.Squat = squat;
         }
 
         public void AddVerticalJump(double verticalJump)
         {
-            _result.VerticalJump = verticalJump;
+            result.VerticalJump = verticalJump;
         }
         public void AddHorizontalJump(double horizontalJump)
         {
-            _result.HorizontalJump = horizontalJump;
+            result.HorizontalJump = horizontalJump;
         }
 
         public Result Build()
         {
-            return _result;
+            return result;
         }
     }
 }
