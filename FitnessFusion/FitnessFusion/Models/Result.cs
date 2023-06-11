1ba230ed523ee0ca3bbe2c62cbc362f83bf02469
epeljto1 <epeljto1@etf.unsa.ba>
2023-06-11 13:34:50 +0200

    Added validation for results and test ciljeva design

--- FitnessFusion/FitnessFusion/Models/Result.cs
+++ FitnessFusion/FitnessFusion/Models/Result.cs
@@ -21,17 +21,24 @@ namespace FitnessFusion.Models
         public Trainer Trainer { get; set; }
         public int Calories { get; set; }
         public DateTime Date { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Mass must be greater than zero.")]
         public double Mass { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Height must be greater than zero.")]
         public double Height { get; set; }
         [DisplayName("Neck Circumference")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Neck circumference must be greater than zero.")]
         public double NeckCircumference { get; set; }
-        [DisplayName("Waist Circumference")] 
+        [DisplayName("Waist Circumference")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Waist circumference must be greater than zero.")]
         public double WaistCircumference { get; set; }
-        [DisplayName("Arm Circumference")] 
+        [DisplayName("Arm Circumference")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Arm circumference must be greater than zero.")]
         public double ArmCircumference { get; set; }
-        [DisplayName("Hips Circumference")] 
+        [DisplayName("Hips Circumference")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Hips circumference must be greater than zero.")]
         public double HipsCircumference { get; set; }
-        [DisplayName("Leg Circumference")] 
+        [DisplayName("Leg Circumference")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Leg circumference must be greater than zero.")]
         public double LegCircumference { get; set; }
         [DisplayName("Bench Press")]
         public double BenchPress { get; set; }
